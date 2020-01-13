7c0dce05567b82fd0f81367e44c897765c33cb88
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-13 16:00:58 -0300

    Upload Project

---
+++ src/main/java/br/com/microservice/auth/SpringMicroserviceAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microservice/auth/SpringMicroserviceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservice.auth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
